b6adc61000b7463a8ecf6e88eab5f02ff08a6b55
kittipoom <kittipoom@MacBook-Air-khxng-kittipoom.local>
2020-12-15 15:40:13 +0700

    เสร็จ

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int N;
+    int Even = 0;
+    int Odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    while (N != 0)
+    {
+        if(N % 2 == 0){
+            Even += 1;
+        }
+        else{
+            Odd += 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> N;
+    }
+    cout << "#Even numbers = " <<  Even << endl;
+    cout << "#Odd numbers = " << Odd << endl;
     return 0;
 }
